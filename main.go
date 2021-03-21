01bb18fb6eddabfd3ec192b79c177dadf8b2ae33
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-21 12:01:08 +0100

    sample on inser, select, and select row

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
   "fmt"
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Tag struct {
+    ID   int    `json:"id"`
+    Name string `json:"name"`
+}
 
 func main() {
   fmt.Println("Go MySQL")
@@ -19,6 +24,7 @@ func main() {
     fmt.Println(" Connected!")
   }
 
+  // Insert record
   insert, err := db.Query("INSERT INTO sample1 (name) VALUES ('Test')")
 
   if err != nil {
@@ -28,6 +34,38 @@ func main() {
   } else {
     fmt.Println(" Record inserted")
   }
+
+  // Select and print records
+  // results, err := db.Query("SELECT id, name FROM sample1")
+  //
+  // if err != nil {
+  //   panic(err.Error())
+  // } else if results.Err() != nil {
+  //   fmt.Println(results.Err().Error())
+  // }
+  //
+  // for results.Next() {
+  //   var tag Tag
+  //
+  //   err = results.Scan(&tag.ID, &tag.Name)
+  //
+  //   if err != nil {
+  //     panic(err.Error())
+  //   }
+  //
+  //   fmt.Println(tag.Name)
+  // }
+
+  // Select just one row
+  // var tag Tag
+  //
+  // err = db.QueryRow("SELECT id, name FROM sample1 WHERE id=?", 2).Scan(&tag.ID, &tag.Name)
+  //
+  // if err != nil {
+  //   panic(err.Error())
+  // }
+  //
+  // fmt.Println(fmt.Sprintf("%v - %v", tag.ID, tag.Name))
 
   defer db.Close()
 }
